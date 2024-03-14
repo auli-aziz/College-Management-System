ca204278dd25884b5efbb46a120c3c580e00acfa
Aulia A Aziz <auliaanugrahaziz@gmail.com>
2024-03-14 16:58:51 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Tugas Kelompok Sistem Basis Data
+Kelompok 3
+---------------
+### Anggota Kelompok
+* Ivander Andreas Wijaya
+* Aulia Anugrah Aziz
+* Zikri Zulfa Azhim
+* Yoel Dwi Miryano
---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+$host = "localhost";
+$port = "5432";
+$dbname = "kuliah";
+$user = "postgres";
+$password = "postgres";
+$pg_options = "--client_encoding=UTF8";
+
+$connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password} options='{$pg_options}'";
+$dbconn = pg_connect($connection_string);
+
+// if($dbconn) {
+// 	echo "Connected to ". pg_host($dbconn);
+// }else {
+// 	echo "Error in connecting to database.";
+// }
+
+?>
